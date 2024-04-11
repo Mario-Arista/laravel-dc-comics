3488e4b92bf305c64733958754ad7a796ffd9d20
mario.arista <mario.arista.dev@gmail.com>
2024-04-11 16:05:41 +0200

    Migrations + Seeder + Model  + ComicController --resource (problem with the format of the column writers and artists in my sql: imported a string as [ "Johnny"...] Solving later this problem)

---
+++ database/migrations/2024_04_11_125805_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price',)->nullable();
+            $table->string('series', 50);
+            $table->string('sale_date', 50);
+            $table->string('type', 50);
+            $table->json('artists');
+            $table->json('writers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
