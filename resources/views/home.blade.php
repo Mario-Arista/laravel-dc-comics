db6f670813a34a39d8114a9178de44224bce5d72
mario.arista <mario.arista.dev@gmail.com>
2024-04-11 14:38:13 +0200

    composer + serve + env + hello world

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts/app')
+
+@section('title')
+    LARAVAL COMICS
+@endsection
+
+@section('content')
+    <div class="container py-5">
+        <h1>ciao</h1>
+    </div>
+@endsection
