703db956109e8b5ed7736227747264d08e2208f0
mario.arista <mario.arista.dev@gmail.com>
2024-04-11 18:04:44 +0200

    Form in the create page (should be correct)

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -15,8 +15,47 @@
     </header>
 
     <div class="container py-5">
-        <form action="">
+        <form action="{{route('comic.store')}}" method="POST">
+            @csrf
+
+            <div class="mb-3">
+              <label for="title" class="form-label">Nome</label>
+              <input type="text" class="form-control" id="title" name="title" required>
+            </div>
+    
+            <div class="mb-3">
+                <label for="series" class="form-label">Serie</label>
+                <input type="text" class="form-control" id="series" name="series" required>
+            </div>
+    
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Src immagine</label>
+                <input type="text" class="form-control" id="thumb" name="thumb" required>
+            </div>
+    
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="text" class="form-control" id="price" name="price">
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <textarea type="text" class="form-control" id="description" name="description"></textarea required>
+            </div>
+
+            <div class="mb-3">
+                <label for="artists" class="form-label">Artisti</label>
+                <textarea type="text" class="form-control" id="artists" name="artists"></textarea required>
+            </div>
+
+            <div class="mb-3">
+                <label for="writers" class="form-label">Scrittori</label>
+                <textarea type="text" class="form-control" id="writers" name="writers"></textarea required>
+            </div>
+    
+            <button type="submit" class="btn btn-primary">Salva</button>
 
         </form>
+
     </div>
 @endsection
