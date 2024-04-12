2cdbf7febdbd662fe62fa0fb6545b95d00947f00
mario.arista <mario.arista.dev@gmail.com>
2024-04-12 15:00:30 +0200

    adding bootstrap js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ import './bootstrap';
 // Import our custom CSS
 import '~resources/scss/app.scss';
 
+// Import Bootstrap js
+import '~bootstrap/js/index.esm.js';
+
 /* Import Bootstrap 5 */
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
