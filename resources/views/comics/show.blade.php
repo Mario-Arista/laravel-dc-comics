e6bc9e211df6f6079b53a7047f7bcedda33bd742
mario.arista <mario.arista.dev@gmail.com>
2024-04-12 17:24:02 +0200

    Completed: Modal with bootstrap

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -30,12 +30,43 @@
         </div>
     </div>
     <div class="container py-4 text-center d-flex justify-content-center">
-        <a href="{{route('comics.edit', $comic->id)}}" class="text-decoration-none bg-danger text text-white p-2 fw-bold p-3 me-3">MODICA FUMETTO</a>
-        <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-            @csrf
-            @method("DELETE")
 
-            <button class="text-decoration-none bg-danger text text-white p-2 fw-bold p-3 border border-0">ELIMINA</button>
-        </form>
+        <a href="{{route('comics.edit', $comic->id)}}" class="text-decoration-none bg-danger text text-white p-2 fw-bold p-3 me-3">MODICA FUMETTO</a>
+        <button type="button" class="btn bg-danger text text-white p-2 fw-bold p-3 me-3 border border-0 rounded-0" data-bs-toggle="modal" data-bs-target="#deleteModal">ELIMINA</button>
+
+    </div>
+
+
+
+    <!-- MODALE BOOTSTRAP -->
+
+    <div class="modal fade bg-dark" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered ">
+            <div class="modal-content">
+
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Elimina la pasta</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+                Sei sicuro che vuoi eliminare questo fumetto? "{{$comic->title}}"
+            </div>
+
+
+            <div class="modal-footer">
+
+                <button type="button" class="btn btn-secondary p-2 fw-bold p-3 border border-0 rounded-0" data-bs-dismiss="modal">ANNULLA</button>
+                <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                    @csrf
+                    @method("DELETE")
+        
+                    <button class="bg-danger text text-white p-2 fw-bold p-3 border border-0">ELIMINA</button>
+                </form>
+
+            </div>
+
+            
+        </div>
     </div>
 @endsection
