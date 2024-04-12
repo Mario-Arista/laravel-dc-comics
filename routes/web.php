3ddc144092a162aecbfe4308967fa0bd45409c6c
mario.arista <mario.arista.dev@gmail.com>
2024-04-12 15:15:52 +0200

    PageController e redirect localhost

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComicController;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', [PageController::class, 'index']);
 
 Route::resource('comics', ComicController::class);
